34d0c1b414fb9a0487c5a8832173aa1ca66cb85e
rkouye <romaric.kouye@opendatasoft.com>
2021-03-01 21:03:33 +0100

    Add webapp with  next

---
+++ griot-webapp/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Griot</title>
+      </Head>
+      <main>Search a quote :</main>
+    </div>
+  );
+}
